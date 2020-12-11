52513e9652205cae2b6a75344dd270639c1e6170
Kieran Glazier <1691215+KieranGlazier@users.noreply.github.com>
2020-12-12 05:29:22 +0800

    Added controllers to the mover and mine objects. The mine will now spawn a number of resources up to a set limit. The mover will collect resources from a mine and take them to a factory

---
+++ Assets/Scripts/MineController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineController : MonoBehaviour
+{
+    private List<GameObject> resourcesInRange  = new List<GameObject>();
+    public GameObject resourcePrefab;
+    private float resourceRange = 2f;
+    private int maxResourcesInRange = 5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider[] resources = Physics.OverlapSphere(transform.position, resourceRange);
+        if (resources.Length < maxResourcesInRange)
+        {
+            spawnResource();
+        }
+        
+    }
+
+
+    private void spawnResource()
+    {
+        float randomX = Random.Range(-resourceRange, resourceRange);
+        float randomZ = Random.Range(-resourceRange, resourceRange);
+        Vector3 randomPosition = transform.position + new Vector3(randomX, 0, randomZ);
+        resourcesInRange.Add(Instantiate(resourcePrefab, randomPosition, Quaternion.identity));
+    }
+}
---
+++ Assets/Scripts/MoverController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private GameObject currentTarget = null;
+    private bool hasTarget = false;
+    private bool carryingResource = false;
+    private Vector3 carryingOffset = new Vector3(0, 2f, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasTarget)
+        {
+            hasTarget = GetNewTarget("Resource");
+        }
+
+        if(hasTarget)
+        {
+            // Move towards the target
+            transform.position = Vector3.MoveTowards(transform.position, currentTarget.transform.position, speed * Time.deltaTime);
+        }
+
+
+    }
+
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+        if (other.CompareTag("Resource"))
+        {
+            // Pick up the resource
+            Vector3 scaleChange = new Vector3(.5f, .5f, .5f);
+            other.gameObject.transform.localScale -= scaleChange;
+            other.transform.position = transform.position + carryingOffset;
+            other.isTrigger = false;
+            other.gameObject.transform.SetParent(transform);
+            // Find a factory to take the resource
+            hasTarget = GetNewTarget("Factory");
+        }
+
+        if (other.CompareTag("Factory"))
+        {
+            if (transform.childCount > 0)
+            {
+                Destroy(transform.GetChild(0).gameObject);
+            }
+            hasTarget = false;
+            currentTarget = null;
+            //transform.DetachChildren();
+            //GetNewTarget("Player Core");
+        }
+    }
+    bool GetNewTarget(string targetTag)
+    {
+        GameObject[] targetList = GameObject.FindGameObjectsWithTag(targetTag);
+        if (targetList.Length > 0)
+        {
+            int randomResourceListPosition = Random.Range(0, targetList.Length - 1);
+            currentTarget = targetList[randomResourceListPosition];
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+}
